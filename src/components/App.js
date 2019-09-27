31f310f6cb9b4ad2b4e321289cfe6255879a9189
Matthew Spector <mspector@verizonmedia.com>
2019-09-27 11:08:46 -0400

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../css/main.css';
+import '../css/base-styles.css';
+
 
 function App() {
   return (
-    <div className="App Bgc(red)">
-    hey
+    <div className="App">
+        <div className="Ta(c)" id="image-main">
+            <div className="H(80%)">
+                <img height="100%" src="beaker.gif" />
+            </div>
+        </div>
     </div>
   );
 }
