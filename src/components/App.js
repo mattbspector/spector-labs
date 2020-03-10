e0f1f5ce9237ee5fa649a967fe605083845b8e83
Matthew Spector <matt.bspector@yahoo.com>
2020-03-09 23:02:30 -0400

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../css/main.css';
 import '../css/base-styles.css';
 
 
 function App() {
+
+
+  const moveBeaker = () => {
+    console.log('hi!');
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+        moveBeaker();
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
         <div className="Ta(c)" id="image-main">
             <div className="H(80%)">
-                <img height="100%" src="beaker.gif" />
+                <img height="100%" src="beaker.gif" id="beakerLoader" />
             </div>
         </div>
     </div>
